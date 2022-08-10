1573c0ec8dccb5ff41e58c2eab12efae1760fb14
Janmejay <janmejay.baghel@cleartrip.com>
2022-08-10 15:36:15 +0530

    OneToOne Mapping between Course and CourseMaterial

---
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.dailycodebuffer.spring.data.jpa.tutorial.repository;
+
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Course;
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+@SpringBootTest
+class CourseRepositoryTest {
+
+    @Autowired
+    private CourseRepository courseRepository;
+    
+    @Test
+    public void printCourses() {
+        List<Course> courses =
+                courseRepository.findAll();
+
+        System.out.println("courses = " + courses);
+    }
+
+}
